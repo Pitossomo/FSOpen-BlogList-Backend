9a28e5e7f4ea1b4965d704cadc38263abcc1e7fb
pitossomo <pitossomo@gmail.com>
2022-02-02 05:27:45 -0300

    Modulation in progress -folders models and utils created

---
+++ models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const mongoUrl = config.MONGODB_URL
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(mongoUrl)
+
+module.exports = Blog
