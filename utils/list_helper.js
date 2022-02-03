d57fa3fdd6345a44074b38bf001ce2da3d27f1da
pitossomo <pitossomo@gmail.com>
2022-02-03 06:38:05 -0300

    Testing with Jest in progress, up to Ex 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,19 @@
 const dummy = (blogs) => 1
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => blog.likes + sum
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteTest = (blogs) => {
+  const reducer = (favBlog, blog) => {
+    const mostLikes = favBlog?.likes || 0
+
+    return (blog.likes > mostLikes ? blog : favBlog)
+  }
+
+  return blogs.reduce(reducer, undefined)
+}
+
+
+module.exports = { dummy, totalLikes, favoriteTest }
