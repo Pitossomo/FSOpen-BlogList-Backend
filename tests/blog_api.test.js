fcf78c37cc6943db73ce3f14bc94ffc5ba01a6be
pitossomo <pitossomo@gmail.com>
2022-02-09 05:41:13 -0300

    Test post method

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,27 +16,83 @@ beforeEach(async () => {
 })
 
 describe('blog api', () => {
-  test('return blogs in JSON format - Ex. 4.8', async () => {
+  test('return blogs in JSON format', async () => {
     await api.get('/api/blogs').expect(200)
       .expect('Content-Type', /application\/json/)
   }, 5000)
   
-  test('return the correct amount of blogs - Ex. 4.8', async () => {
+  test('return the correct amount of blogs', async () => {
     const response = await api.get('/api/blogs')
     
     expect(response.body).toHaveLength(6)
   })
 
-  test('has the unique identifier named as id - Ex. 4.9', async () => {
+  test('has the unique identifier named as id', async () => {
     const response = await api.get('/api/blogs')
     const firstBlog = response.body[0]
 
     expect(firstBlog.id).toBeDefined()
   })
 
-  test.todo('Should add 1 to the total ammount of blogs when posting a new blog - Ex. 4.10')
-  test.todo('Should save correctly the content of the blog to the database - Ex. 4.10')
-  test.todo('Should default value of likes to 0 when it is missing - Ex. 4.11')
+  // Exercise 4.10
+  test('add 1 to the ammount of blogs when posting a new blog', async () => {
+    const blogToPost = {
+      title: "React is fun",
+      author: "Pitossomo",
+      url: "pitoact.com"
+    }
+
+    // POST to the database
+    await api.post('/api/blogs').send(blogToPost).expect(201)
+
+    // GET from database and check for increased length
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(7)
+
+
+  })
+
+  // Exercise 4.10
+  test.only('save correctly the content of the blog - Ex. 4.10', async () => {
+    const blogToPost = {
+      title: 'React is fun',
+      author: 'Pitossomo',
+      url: 'pitoact.com'
+    }
+
+    // POST to the database
+    await api.post('/api/blogs').send(blogToPost).expect(201)
+
+    // GET from database and check content
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(blog => ({
+      title: blog.title, 
+      author: blog.author, 
+      url: blog.url
+    }))
+
+    expect(contents).toContainEqual({
+      title: 'React is fun',
+      author: 'Pitossomo',
+      url: 'pitoact.com'
+    })
+  })
+
+
+  // Exercise 4.11
+  test('default value of likes to 0 when it is missing', async () => {
+    const blogToPost = {
+      title: "React is fun",
+      author: "Pitossomo",
+      url: "pitoact.com"
+    }
+
+    const response = await api.post('/api/blogs').send(blogToPost)
+
+    const blog = response.body
+    expect(blog.likes).toBe(0)
+  })
+
   test.todo('Should return 400 error when posting new blog without title or url - Ex. 4.12')
 })
 
